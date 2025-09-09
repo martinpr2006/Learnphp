9bf96173e5ef6fef37e988bb3d5909df500bf91b
Martin Pruus <Martin.Pruus@tptlive.ee>
2025-09-09 13:01:56 +0300

    Update class

--- class.php
+++ class.php
@@ -1,20 +1,56 @@
 <?php
 
-// blueprint
 class Box {
-    public $width;
-    public $height;
-    public $length;
+    private $width;
+    protected $heigth;
+    private $length;
+    
+    public function __construct($w=0, $h=0, $l=0)
+    {   
+        $this->width = $w;
+        $this->heigth = $h;
+        $this->length = $l;
+    }
 
-    public function volume() {
-        return $this -> width * $this -> heigh * $this -> length;
+    public function getWidth(){
+        return $this->width;
+    }
+
+    public function setWidth($width){
+        if($width > 0 && is_numeric($width)){
+            $this->width = $width;
+        } else {
+            throw new Exception('You are stupid dumbass');
+        }
+
+        
+    }
+
+    public function volume(){
+        return $this->width * $this->heigth * $this->length;
     }
 }
 
-$box1 = new Box();
-$box1 -> width = 10;
-$box1 -> height = 10;
-$box1 -> length = 10;
+class MetalBox extends Box {
+    public $material = 'metal🤘';
+    public $massPerUnit = 2; 
+    
+    public function changeWidth(){
+        $this->width = 333;
+    }
 
-var_dump($box1);
-var_dump($box1 -> volume());
+    public function mass() {
+        return $this->volume() * $this->massPerUnit;
+    }
+}
+
+$metalBox = new Box(2,3,4);
+//$metalBox->width = 'Cool value';
+$metalBox->setWidth(12);
+//$metalBox->width = 123;
+//$metalBox->changeWidth();
+//var_dump($metalBox->width);
+
+var_dump($metalBox);
+var_dump($metalBox->volume());
+//var_dump($metalBox->mass());
