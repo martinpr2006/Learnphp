83ac82b309b12abee326dd05a34ad4c5ddb6d02a
Martin Pruus <martin.pruus@tptlive.ee>
2025-09-30 16:07:54 +0300

    Add database

--- routes.php
+++ routes.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Controllers\PostsController;
 use App\Controllers\PublicController;
 use App\Router;
 
 Router::get('/', [PublicController::class, 'index']);
-
 Router::get('/us', [PublicController::class, 'us']);
 Router::get('/form', [PublicController::class, 'form']);
 Router::post('/answer', [PublicController::class, 'answer']);
+Router::get('/posts', [PostsController::class, 'index']);
